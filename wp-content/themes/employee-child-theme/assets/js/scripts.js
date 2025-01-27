5a249a03656704ea1e941e71cfb019090dc3c248
Josh Espinoza <joshespi@gmail.com>
2025-01-27 11:18:13 -0700

    formatting

--- wp-content/themes/employee-child-theme/assets/js/scripts.js
+++ wp-content/themes/employee-child-theme/assets/js/scripts.js
@@ -3,9 +3,9 @@
 accordion
 =============================================================================================================
 */
-jQuery(".accordian-category").click( function(){
-	jQuery(this).toggleClass("active");
-	jQuery(this).next(".accordian-content").toggleClass("active");
+jQuery(".accordian-category").click(function () {
+  jQuery(this).toggleClass("active");
+  jQuery(this).next(".accordian-content").toggleClass("active");
 });
 /*
 =============================================================================================================
@@ -13,27 +13,26 @@ Directory Live Page Search
 =============================================================================================================
 */
 
-jQuery(document).ready(function(){
-    jQuery("#appfilter").keyup(function(){
+jQuery(document).ready(function () {
+  jQuery("#appfilter").keyup(function () {
+    // Retrieve the input field text and reset the count to zero
+    var filter = jQuery(this).val(),
+      count = 0;
 
-        // Retrieve the input field text and reset the count to zero
-        var filter = jQuery(this).val(), count = 0;
+    // Loop through the post list
+    jQuery(".approvedApps .post").each(function () {
+      // If the list item does not contain the text phrase fade it out
+      if (jQuery(this).text().search(new RegExp(filter, "i")) < 0) {
+        //jQuery(this).addClass('hide');
+        jQuery(this).fadeOut();
 
-        // Loop through the post list
-        jQuery(".approvedApps .post").each(function(){
-
-            // If the list item does not contain the text phrase fade it out
-            if (jQuery(this).text().search(new RegExp(filter, "i")) < 0) {
-                 //jQuery(this).addClass('hide');
-                jQuery(this).fadeOut();
-
-            // Show the list item if the phrase matches and increase the count by 1
-            } else {
-                jQuery(this).show();
-                count++;
-            }
-        });
+        // Show the list item if the phrase matches and increase the count by 1
+      } else {
+        jQuery(this).show();
+        count++;
+      }
     });
+  });
 });
 /*
 =============================================================================================================
@@ -41,39 +40,38 @@ Auto Link Detection
 =============================================================================================================
 */
 
-jQuery('.content ul li a').each(function(){
-    if (jQuery(this).attr('href').match('.pdf')) {
-        jQuery(this).parent().addClass('pdf');
-    } else if(jQuery(this).attr('href').match('.xls')) {
-        jQuery(this).parent().addClass('xls');
-    } else if (jQuery(this).attr('href').match('provo.edu')) {
-        jQuery(this).parent().addClass('int');
-    } else {
-        jQuery(this).parent().addClass('ext');
-    }
+jQuery(".content ul li a").each(function () {
+  if (jQuery(this).attr("href").match(".pdf")) {
+    jQuery(this).parent().addClass("pdf");
+  } else if (jQuery(this).attr("href").match(".xls")) {
+    jQuery(this).parent().addClass("xls");
+  } else if (jQuery(this).attr("href").match("provo.edu")) {
+    jQuery(this).parent().addClass("int");
+  } else {
+    jQuery(this).parent().addClass("ext");
+  }
 });
 /*
 =============================================================================================================
 sort list alpha by giving it the class sortList
 =============================================================================================================
 */
-jQuery(window).on("load", function() {
-	var elem = jQuery('.sortList'); //replace this with your list selector
-	sortList(elem);
-	function sortList($elem) {
-		var $li = $elem.find('li'),
-			$listLi = jQuery($li,$elem).get();
-		$listLi.sort(function(a, b){
-			var keyA = jQuery(a).text().toUpperCase();
-			var keyB = jQuery(b).text().toUpperCase();
-			return (keyA < keyB) ? -1 : 1;
-		});
-		jQuery.each($listLi, function(index, row){
-			$elem.append(row);
-		});
-	}
+jQuery(window).on("load", function () {
+  var elem = jQuery(".sortList"); //replace this with your list selector
+  sortList(elem);
+  function sortList($elem) {
+    var $li = $elem.find("li"),
+      $listLi = jQuery($li, $elem).get();
+    $listLi.sort(function (a, b) {
+      var keyA = jQuery(a).text().toUpperCase();
+      var keyB = jQuery(b).text().toUpperCase();
+      return keyA < keyB ? -1 : 1;
+    });
+    jQuery.each($listLi, function (index, row) {
+      $elem.append(row);
+    });
+  }
 });
-
 
 //inserts a link to the translation request form in the language switcher
 window.onload = function () {
@@ -84,18 +82,24 @@ window.onload = function () {
     newAnchor.textContent = "Request Translation"; // set link text
     wrapper.insertBefore(newAnchor, wrapper.children[1]); //insert new anchor before first child
 
-    var targetElement = wrapper.querySelector(".trp-floater-ls-disabled-language.trp-ls-disabled-language"); // find the target element
+    var targetElement = wrapper.querySelector(
+      ".trp-floater-ls-disabled-language.trp-ls-disabled-language"
+    ); // find the target element
     if (targetElement) {
       targetElement.textContent += " - Selected"; // append "current lang" to the existing text
     }
   }
-  var parentElement = document.getElementById('trp-floater-ls-current-language'); // get the parent element by ID
+  var parentElement = document.getElementById(
+    "trp-floater-ls-current-language"
+  ); // get the parent element by ID
   if (parentElement) {
-    var targetElement = parentElement.querySelector('.trp-floater-ls-disabled-language.trp-ls-disabled-language'); // find the target element inside the parent
+    var targetElement = parentElement.querySelector(
+      ".trp-floater-ls-disabled-language.trp-ls-disabled-language"
+    ); // find the target element inside the parent
     if (targetElement) {
-      var img = document.createElement('img'); // create new img element
-      img.src = 'https://provo.edu/wp-content/uploads/2024/01/translate.png'; // set src attribute
-      targetElement.innerHTML = ''; // clear the current content
+      var img = document.createElement("img"); // create new img element
+      img.src = "https://provo.edu/wp-content/uploads/2024/01/translate.png"; // set src attribute
+      targetElement.innerHTML = ""; // clear the current content
       targetElement.appendChild(img); // append the new image
     }
   }
